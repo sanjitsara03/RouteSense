58586d4f31d1f2fe7ac2b4bb812e68ad57f41f73
sanjit369 <sanjit2003@gmail.com>
2025-05-22 22:42:44 +0530

    Added requirements and set config for spark

---
+++ jobs/spark-jobs.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession, DataFrame
+
+from config import configuration
+
+
+def main():
+    spark = SparkSession.builder.appName("RouteSenseStreaming") \
+        .config("spark.jars.packages",
+                "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.5,"
+                "org.apache.hadoop:hadoop-a ws:3.4.1,"
+                "com.amazonaws:aws-java-sdk:1.11.469") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.access.key", configuration.get('AWS_ACCESS_KEY')) \
+        .config("spark.hadoop.fs.s3a.secret.key", configuration.get('AWS_SECRET_KEY')) \
+        .config('spark.hadoop.fs.s3a.aws.credentials.provider',
+                'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
+        .getOrCreate()
+
+
+    spark.sparkContext.setLogLevel('WARN')
+
+
+if __name__ == "__main__":
+    main()
