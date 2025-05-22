aba4e8277614fb24e2c3c0a70e48520bb1799e4f
sanjit369 <sanjit2003@gmail.com>
2025-05-22 22:48:58 +0530

    Added data schema

--- jobs/spark-jobs.py
+++ jobs/spark-jobs.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession, DataFrame
-
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType, DoubleType
 from config import configuration
 
 
@@ -16,8 +16,63 @@ def main():
                 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
         .getOrCreate()
 
-
     spark.sparkContext.setLogLevel('WARN')
+
+    vehicle = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceId", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("location", StringType(), True),
+        StructField("speed", DoubleType(), True),
+        StructField("direction", StringType(), True),
+        StructField("make", StringType(), True),
+        StructField("model", StringType(), True),
+        StructField("year", IntegerType(), True),
+        StructField("fuelType", StringType(), True),
+    ])
+
+
+    gps = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceId", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("speed", DoubleType(), True),
+        StructField("direction", StringType(), True),
+        StructField("vehicleType", StringType(), True)
+    ])
+
+    traffic = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceId", StringType(), True),
+        StructField("cameraId", StringType(), True),
+        StructField("location", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("snapshot", StringType(), True)
+    ])
+
+    weather = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceId", StringType(), True),
+        StructField("location", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("temperature", DoubleType(), True),
+        StructField("weatherCondition", StringType(), True),
+        StructField("precipitation", DoubleType(), True),
+        StructField("windSpeed", DoubleType(), True),
+        StructField("humidity", IntegerType(), True),
+        StructField("airQualityIndex", DoubleType(), True),
+    ])
+
+    emergency = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceId", StringType(), True),
+        StructField("incidentId", StringType(), True),
+        StructField("type", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("location", StringType(), True),
+        StructField("status", StringType(), True),
+        StructField("description", StringType(), True),
+    ])
 
 
 if __name__ == "__main__":
