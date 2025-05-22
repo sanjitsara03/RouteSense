d332c35a3d117947db5d2761d93d0fcde98914a5
sanjit369 <sanjit2003@gmail.com>
2025-05-22 18:45:48 +0530

    Added data generation to main

---
+++ jobs/main.py
@@ -0,0 +1,110 @@
+import os
+import random
+import uuid
+
+from datetime import datetime, timedelta
+
+STATECOLLEGE_COORDINATES = {"latitude": 40.7934, "longitude": 77.8600}
+NEWYORK_COORDINATES = {"latitude": 40.7128, "longitude": 74.0060}
+
+LATITUDE_INCREMENT = (NEWYORK_COORDINATES['latitude'] - STATECOLLEGE_COORDINATES['latitude']) / 100
+LONGITUDE_INCREMENT = (NEWYORK_COORDINATES['longitude'] - STATECOLLEGE_COORDINATES['longitude']) / 100
+
+# Environment Variables for configuration
+KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
+VEHICLE_TOPIC = os.getenv('VEHICLE_TOPIC', 'vehicle_data')
+GPS_TOPIC = os.getenv('GPS_TOPIC', 'gps_data')
+TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
+WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_data')
+EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
+
+random.seed(42)
+start_time = datetime.now()
+start_location = STATECOLLEGE_COORDINATES.copy()
+
+
+def get_next_time():
+    global start_time
+    start_time += timedelta(seconds=random.randint(30, 60))
+    return start_time
+
+
+def generate_gps_data(device_id, timestamp, vehicle_type='private'):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'timestamp': timestamp,
+        'speed': random.uniform(0, 40),  # km/h
+        'direction': 'North-East',
+        'vehicleType': vehicle_type
+    }
+
+
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'cameraId': camera_id,
+        'location': location,
+        'timestamp': timestamp,
+        'snapshot': 'Base64EncodedString'
+    }
+
+
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'location': location,
+        'timestamp': timestamp,
+        'temperature': random.uniform(-5, 26),
+        'weatherCondition': random.choice(['Sunny', 'Cloudy', 'Rain', 'Snow']),
+        'precipitation': random.uniform(0, 25),
+        'windSpeed': random.uniform(0, 100),
+        'humidity': random.randint(0, 100),  # percentage
+        'airQualityIndex': random.uniform(0, 500)  # AQL Value goes here
+    }
+
+
+def generate_emergency_incident_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'incidentId': uuid.uuid4(),
+        'type': random.choice(['Accident', 'Fire', 'Medical', 'Police', 'None']),
+        'timestamp': timestamp,
+        'location': location,
+        'status': random.choice(['Active', 'Resolved']),
+        'description': 'Description of the incident'
+    }
+
+
+def simulate_vehicle_movement():
+    global start_location
+    # move towards new york
+    start_location['latitude'] += LATITUDE_INCREMENT
+    start_location['longitude'] += LONGITUDE_INCREMENT
+
+    # randomness to simulate actual road travel
+    start_location['latitude'] += random.uniform(-0.0005, 0.0005)
+    start_location['longitude'] += random.uniform(-0.0005, 0.0005)
+
+    return start_location
+
+
+def generate_vehicle_data(device_id):
+    location = simulate_vehicle_movement()
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'timestamp': get_next_time().isoformat(),
+        'location': (location['latitude'], location['longitude']),
+        'speed': random.uniform(10, 40),
+        'direction': 'North-East',
+        'make': 'BMW',
+        'model': 'C500',
+        'year': 2024,
+        'fuelType': 'Hybrid'
+    }
+
+
